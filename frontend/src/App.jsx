9f016f3591d86b72b9a7c6951ffa7f8fe218b70d
Yuvraj <fcyuvraj18@gmail.com>
2026-04-10 11:40:54 +0530

    url fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const handleSubmit = async () => {
     console.log("Sending code:", code)
 
     const res = await axios.post(
-      import.meta.env.VITE_BACKEND_URL,
+      `${import.meta.env.VITE_BACKEND_URL}/ai/get-response`,
       { code },
       { timeout: 10000 }
     )
