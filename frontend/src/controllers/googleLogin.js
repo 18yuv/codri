8fde65fcc7cd97291fb6b409a8bb896938ed940a
Yuvraj <fcyuvraj18@gmail.com>
2026-05-07 11:28:48 +0530

    google login bug fix

--- frontend/src/controllers/googleLogin.js
+++ frontend/src/controllers/googleLogin.js
@@ -1,13 +1,21 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    setPersistence,
+    browserLocalPersistence
+} from "firebase/auth";
 import { auth } from "../services/firebase.js";
 import { googleProvider } from "../services/firebase.js";
 
 const googleLogin = async () => {
     try {
+        await setPersistence(auth, browserLocalPersistence);
         const result = await signInWithPopup(auth, googleProvider);
         const token = await result.user.getIdToken(true);
+        return result.user;
     } catch (err) {
         console.log(err)
+        throw err;
     }
 };
 
