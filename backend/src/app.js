0a583a6e39101c77a896ef8651224a991c729771
Yuvraj <fcyuvraj18@gmail.com>
2026-05-07 10:29:15 +0530

    server deploy fail bug fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,8 @@ import 'dotenv/config';
 import aiRouter from "./routes/ai.routes.js";
 import cors from "cors";
 import authRouter from "./routes/authRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 export const app = express()
 app.use(cors({
   origin: process.env.FRONTEND_URL,
@@ -21,11 +18,6 @@ app.get('/', (req, res)=>{
 
 app.use('/ai', aiRouter);
 app.use('/api', authRouter)
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 app.use((req, res) => {
   return res.status(404).json({message: 'Endpoint not found'})
